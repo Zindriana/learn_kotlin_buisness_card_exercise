02e836c3cf799775bb35cf50cb4f80dd6651d577
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-12-18 16:33:37 +0100

    Created the repository

---
+++ app/src/main/java/com/example/kotlin_business_card_exercise/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.kotlin_business_card_exercise
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlin_business_card_exercise.ui.theme.Kotlin_business_card_exerciseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Kotlin_business_card_exerciseTheme {
+
+            }
+        }
+    }
+}
+
