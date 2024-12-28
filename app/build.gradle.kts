b5f7512153459c41a5f7ab0c2909d0decc996ae9
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2024-12-28 19:07:27 +0100

    Added text to the ContactInfo Composable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.kotlin_business_card_exercise"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.kotlin_business_card_exercise"
--- app/src/main/java/com/example/kotlin_business_card_exercise/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_business_card_exercise/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlin_business_card_exercise.ui.theme.Kotlin_business_card_exerciseTheme
 
@@ -19,8 +25,59 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Kotlin_business_card_exerciseTheme {
-
+                Column {
+                    Column {
+                        LogoImage()
+                        TitleCard(title = "Testnamn", name = "testtitel")
+                    }
+                    Column {
+                        ContactInfo(phoneNumber = "0712345678", socialMedia = "testur", email = "test@test.se")
+                    }
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun LogoImage() {
+    val image = painterResource(R.drawable.android_logo)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+}
+
+
+@Composable
+fun TitleCard(title: String, name: String) {
+    Column {
+        Text(
+            text = name
+        )
+        Text(
+            text = title
+        )
+    }
+}
+
+@Composable
+fun ContactInfo(phoneNumber: String, socialMedia: String, email: String) {
+    Column {
+        Row {
+            Text(
+                text = phoneNumber
+            )
+        }
+        Row {
+            Text(
+                text = socialMedia
+            )
+        }
+        Row {
+            Text(
+                text = email
+            )
         }
     }
 }
